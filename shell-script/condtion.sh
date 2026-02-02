7b5bd3ed3a2b492dda167ba1cb5219bc0cce66bd
bhanuparkash <bhanuyalavarthi999@gmail.com>
2026-02-02 22:19:14 +0530

    shell script practice

---
+++ shell-script/condtion.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+NUMBER=$100
+
+# -gt -> greater than
+# -lt -> less than
+# -eq -> equal
+# -ne -> not equal
+
+if [$NUMBER -gt 150]; then
+    echo "give number: $NUMBER is greater than 150"
+
+ else
+     echo "give number: $NUMBER is less than 150"
+fi
