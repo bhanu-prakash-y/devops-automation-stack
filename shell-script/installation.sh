ef32f007c19498468f9137b0242dfa0b82131f9a
bhanuparkash <bhanuyalavarthi999@gmail.com>
2026-02-02 22:50:45 +0530

    shell script practice

--- shell-script/installation.sh
+++ shell-script/installation.sh
@@ -17,3 +17,25 @@ if [ $? -ne 0]; then
 else    
  echo "Installing Ngnix........SUCCESS"
 fi 
+
+
+dnf install mysql -y
+
+if [ $? -ne 0]; then
+    echo "Installing MYSQL........FAILURE"
+    exit 1
+
+else    
+ echo "Installing MYSQL........SUCCESS"
+fi 
+
+
+dnf install nodejs -y
+
+if [ $? -ne 0]; then
+    echo "Installing nodejs........FAILURE"
+    exit 1
+
+else    
+ echo "Installing nodejs........SUCCESS"
+fi 
