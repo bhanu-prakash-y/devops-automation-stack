a24d3680b6f3d29f290e2ce47d8dbfac16eff05a
bhanuparkash <bhanuyalavarthi999@gmail.com>
2026-02-03 22:39:27 +0530

    shell script practice

---
+++ shell-script/errorhanding.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+trap 'echo" there is an eror in $LINEND, Command :$BASH_COMMAND"' ERR 
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "please run this script root acccess" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+for Package in  $@    #sudo loop.sh nodejs mysql
+do
+   dnf list  installed  $Packages &>>$LOGS_FILE
+  if [ $? -ne 0 ]; then
+     echo "$Package not installed, install now"
+     dnf install $Package -y &>>$LOGS_FILE
+    else 
+      echo "$Package already installed ..... SKIPPING" 
+   fi
+done
