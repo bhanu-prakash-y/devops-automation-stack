31e77eb486ffc976cb2844d2a90639b29b3b54d5
bhanuparkash <bhanuyalavarthi999@gmail.com>
2026-02-04 21:48:16 +0530

    robo shell

--- robo-shell/roboshop.sh
+++ robo-shell/roboshop.sh
@@ -6,19 +6,19 @@ AMIID="ami-0220d79f3f480ecf5"
 
 for instance in $@
 do
-   Instance_id=$( aws ec2 run-instances \
+   INSTANCE_ID=$( aws ec2 run-instances \
     --image-id $AMIID \
     --instance-type "t3.micro" \
     --security-group-ids $SG_ID \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-    --query 'Instances[0].insatnaceid' \
+    --query 'Instances[0].InstanceId' \
     --output text )
 
      if [ $instance == "forntend" ]; then
 
          IP=$(
             aws ec2 describe-instances \
-            --instance-ids $Instance_id \
+            --instance-ids $INSTANCE_ID \
             --query 'Reservations[].Instances[].PublicIpAddress' \
             --output text
          )
@@ -27,7 +27,7 @@ do
 
           IP=$(
             aws ec2 describe-instances \
-            --instance-ids $Instance_id \
+            --instance-ids $INSTANCE_ID \
             --query 'Reservations[].Instances[].PrivateIpAddress' \
             --output text
             
