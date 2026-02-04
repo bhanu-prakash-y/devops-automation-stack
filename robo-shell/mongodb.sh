510b3e2e5be1949c181c9b07e8f1a4ec08e772ca
bhanuparkash <bhanuyalavarthi999@gmail.com>
2026-02-04 23:48:23 +0530

    robo shell

---
+++ robo-shell/mongodb.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+
+
+if [ $USERID -ne 0 ]; then
+    echo  "please run this script root acccess" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+         echo "$2 ... FAILURE" | tee -a $LOGS_FILE
+         exit 1
+    else    
+      echo  "$2 ... SUCCESS"    | tee -a $LOGS_FILE
+    fi 
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copying Mongo Repo"
+
+dnf install mongodb-org -y 
+VALIDATE $? "Installing mongoDB server"
+
+systemctl enable mongod 
+VALIDATE $? "Enable MongoDB"
+
+systemctl start mongod 
+VALIDATE $? "Start MongoB"
+
+ sed -i 's/127.0.0.1 to 0.0.0.0/' /etc/mongod.conf
+ VALIDATE $? "Allowing remote connections"
+
+ systemctl start mongod 
+ VALIDATE $? "ReStart MongoB"
