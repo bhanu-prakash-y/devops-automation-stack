80b1992503a998d40e18e19b4b773810ab3f6b11
bhanuparkash <bhanuyalavarthi999@gmail.com>
2026-02-04 16:18:42 +0530

    shell script

--- shell-script/errorhanding.sh
+++ shell-script/errorhanding.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+
 set -e
-trap 'echo "There is an error in $LINENO, Command: $BASH_COMMAND"' ERR 
+trap 'echo "There is an error in $LINEND, Command: $BASH_COMMAND"' ERR 
 
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0.log"
 
 if [ $USERID -ne 0 ]; then
-    echo "please run this script root acccess" | tee -a $LOGS_FILE
+    echo "Please run this script with root user  acccess" | tee -a $LOGS_FILE
     exit 1
 fi
 
@@ -16,20 +17,17 @@ mkdir -p $LOGS_FOLDER
 for Package in $@
 
 do
-   dnf list  installed  $Packages &>>$LOGS_FILE
+    dnf list  installed  $Packages &>>$LOGS_FILE
 
   if [ $? -ne 0 ]; then
 
-     echo "$Package not installed, installing now"
+       echo "$Package not installed, installing now"
 
-     dnf install $Package -y &>>$LOGS_FILE
+       dnf install $Package -y &>>$LOGS_FILE
 
-    else
+   else
     
-      echo "$Package already installed ... SKIPPING" 
+       echo "$Package already installed ... SKIPPING" 
    fi
+   
 done
-
-
-
-
