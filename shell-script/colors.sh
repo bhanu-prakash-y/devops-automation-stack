7cda001cf2119d940473d9426ece42d927a2f8e4
bhanuparkash <bhanuyalavarthi999@gmail.com>
2026-02-04 14:27:25 +0530

    shell script practice

--- shell-script/colors.sh
+++ shell-script/colors.sh
@@ -17,11 +17,12 @@ fi
 mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
+
     if [ $1 -ne 0 ]; then
          echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
          exit 1
-    else    
-      echo -e "$2 ... $G SUCCESS $N"    | tee -a $LOGS_FILE
+    else 
+      echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi 
 }
 
