8af5aad974de8b8b3436a0028b72b5ef93d27b64
bhanuparkash <bhanuyalavarthi999@gmail.com>
2026-02-05 00:26:38 +0530

    robo shell

--- robo-shell/mongodb.sh
+++ robo-shell/mongodb.sh
@@ -33,6 +33,11 @@ VALIDATE $? "Enable MongoDB"
 systemctl start mongod 
 VALIDATE $? "Start MongoB"
 
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+ VALIDATE $? "Allowing remote connections"
+
+ systemctl restart mongod
+ VALIDATE $? "ReStart MongoB"
 
 
  
