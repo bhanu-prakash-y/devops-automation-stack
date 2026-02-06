651a1e9af23a40e21a22f1e18950fdf301df9855
bhanuparkash <bhanuyalavarthi999@gmail.com>
2026-02-06 14:34:49 +0530

    robo shell

--- robo-shell/mysql.sh
+++ robo-shell/mysql.sh
@@ -23,7 +23,7 @@ VALIDATE(){
 
 
 dnf install mysql-server -y &>>$LOGS_FILE
-VALIDATE $? "Install mysql"
+VALIDATE $? "Installing  mysql"
 
 systemctl enable mysqld &>>$LOGS_FILE
 systemctl start mysqld  
