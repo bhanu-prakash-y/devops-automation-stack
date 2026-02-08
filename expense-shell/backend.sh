f7232b7648c7b69acd98dabe8c9078eb22223999
bhanuparkash <bhanuyalavarthi999@gmail.com>
2026-02-08 17:27:07 +0530

    expense

--- expense-shell/backend.sh
+++ expense-shell/backend.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-roboshop"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
 script_dir=$PWD
-mongodb_host=mongodb.bhanudevops.online
+mysqldb_host=mysql.bhanudevops.online
 
 
 if [ $USERID -ne 0 ]; then
@@ -34,18 +34,21 @@ VALIDATE $? "Enable NodeJS"
 dnf install nodejs -y &>>$LOGS_FILE
 VALIDATE $? "Install NodeJS"
 
+dnf update -y openssh openssh-server openssh-clients &>>$LOGS_FILE
+VALIDATE $? "update openssh"
+
 id roboshop  &>>$LOGS_FILE
 if [ $? -ne 0 ]; then 
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop  &>>$LOGS_FILE
     VALIDATE $? "Creating user"
 else 
-     echo "Roboshop user already exit ... SKIPPING"
+     echo " user already exit ... SKIPPING"
 fi     
 
 mkdir -p /app 
 VALIDATE $? "Creating app dir"
 
-curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip  &>>$LOGS_FILE
+curl -o /tmp/backend.zip https://expense-joindevops.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGS_FILE
 VALIDATE $? "Downloading user code"
 
 cd /app 
@@ -54,25 +57,27 @@ VALIDATE $? "moving to app directory"
 rm -rf /app/*
 VALIDATE $? "remove the exit code"
 
-unzip /tmp/user.zip &>>$LOGS_FILE
+unzip /tmp/backend.zip &>>$LOGS_FILE
 VALIDATE $? "Unzip user code"
 
 
 npm install  &>>$LOGS_FILE
 VALIDATE $? "Installing dependencies"
 
-cp $script_dir/user.service /etc/systemd/system/user.service
+cp $script_dir/backend.service /etc/systemd/system/backend.service
 VALIDATE $? "created systemctl service"
 
 systemctl daemon-reload
-systemctl enable user 
-systemctl start user
-VALIDATE $? "starting and enabling user"
+systemctl start backend 
+systemctl enable backend
+VALIDATE $? "starting and enabling backend"
 
 
+dnf install mysql -y
+VALIDATE $? "installing"
+
+mysql -h $mysqldb_host -uroot -pExpenseApp@1 < /app/schema/backend.sql
 
 
-
-
-
-
+systemctl restart backend
+VALIDATE $? "restart backend"
